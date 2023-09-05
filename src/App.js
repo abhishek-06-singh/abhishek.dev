ee1d97b5246510b5e12cf4455238a118eae61055
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-09-06 01:01:14 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from "./home/home";
 import Loading from "./landing_page/loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+
   return (
     <div className="App">
-      <Loading />
+      <Router>
+        <Switch>
+          {/* Route for the loading screen */}
+          <Route path="/loading">
+            {loading ? <Loading /> : <Redirect to="/home" />}
+          </Route>
+
+          {/* Route for the home page */}
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -4,7 +4,7 @@ import Loading from "../landing_page/loading";
 const Home = () => {
   return (
     <div>
-      <Loading />
+      <div style={{ fontSize: "large", color: "red" }}>hello</div>
     </div>
   );
 };
