d402862ab56b7c418645deb2cc7adee738768212
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-09-06 10:14:04 +0530

    corrected route mistakes

--- src/App.js
+++ src/App.js
@@ -9,25 +9,11 @@ import Home from "./home/home";
 import Loading from "./landing_page/loading";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2500);
-  }, []);
-
   return (
     <div className="App">
       <Router>
         <Switch>
-          {/* Route for the loading screen */}
-          <Route path="/loading">
-            {loading ? <Loading /> : <Redirect to="/home" />}
-          </Route>
-
-          {/* Route for the home page */}
-          <Route path="/home" component={Home} />
+          <Route path="/" component={Home} />
         </Switch>
       </Router>
     </div>
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "../landing_page/loading";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <div style={{ fontSize: "large", color: "red" }}>hello</div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div style={{ fontSize: "large", color: "red" }}>hello</div>
+      )}
     </div>
   );
 };
